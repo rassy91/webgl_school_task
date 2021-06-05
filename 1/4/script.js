7cd033f91934c8d837d3f7ef9e134141dfe5d7ff
igarashi <digarashi91@gmail.com>
2021-06-05 13:53:29 +0900

    [add] 4 初期描写

--- 1/4/script.js
+++ 1/4/script.js
@@ -5,9 +5,103 @@
 
     document.addEventListener('DOMContentLoaded', () => {
 
+        init();
 
+        setEvent();
+
+        render();
 
     });
+
+    // DOM
+    let wrapper;
+
+    // flags
+    let escape = false;
+
+    // three.js 用の変数
+    let scene;
+    let camera;
+    let renderer;
+    let geometry;
+    let material;
+    let box;
+    let controls;
+
+    const CAMERA_PARAM = {
+        fovy: 60,
+        aspect: window.innerWidth / window.innerHeight,
+        near: 0.1,
+        far: 20,
+        x: 1.0,
+        y: 1.0,
+        z: 5.0,
+        lookAt: new THREE.Vector3(0.0, 0.0, 0.0)
+    };
+    const RENDERER_PARAM = {
+        clearColor: 0x333333,
+        width: window.innerWidth,
+        height: window.innerHeight
+    };
+    const MATERIAL_PARAM = {
+        color: 0x7777ff
+    };
+
+    function init() {
+
+        wrapper = document.getElementById('webgl');
+
+        scene = new THREE.Scene();
+
+        renderer = new THREE.WebGLRenderer();
+        renderer.setClearColor(RENDERER_PARAM.clearColor);
+        renderer.setSize(RENDERER_PARAM.width, RENDERER_PARAM.height);
+        wrapper.appendChild(renderer.domElement);
+
+        camera = new THREE.PerspectiveCamera(
+            CAMERA_PARAM.fovy,
+            CAMERA_PARAM.aspect,
+            CAMERA_PARAM.near,
+            CAMERA_PARAM.far
+        );
+        camera.position.set(
+            CAMERA_PARAM.x,
+            CAMERA_PARAM.y,
+            CAMERA_PARAM.z,
+        );
+        camera.lookAt(CAMERA_PARAM.lookAt);
+
+        geometry = new THREE.BoxGeometry(1.0, 1.0, 1.0);
+        material = new THREE.MeshBasicMaterial(MATERIAL_PARAM);
+
+        box = new THREE.Mesh(geometry, material);
+        scene.add(box);
+
+    }
+
+    function render() {
+
+        if (escape) {
+            return;
+        }
+
+        requestAnimationFrame(render)
+
+        renderer.render(scene, camera);
+
+    }
+
+
+
+    function setEvent() {
+
+        document.addEventListener('keydown', (e) => {
+
+            escape = e.code === 'Escape';
+
+        });
+
+    }
 
 
 })();
