7e066c4c249334e4e1a946b7524e63c7f69c8dc1
igarashi <digarashi91@gmail.com>
2021-06-14 23:51:02 +0900

    [modify] リサイズイベントを追加

--- 2/exercise/script.js
+++ 2/exercise/script.js
@@ -217,6 +217,14 @@
 
         });
 
+        window.addEventListener('resize', () => {
+            renderer.setSize(window.innerWidth, window.innerHeight);
+
+            camera.aspect = window.innerWidth / window.innerHeight;
+
+            camera.updateProjectionMatrix();
+        });
+
     }
 
 
