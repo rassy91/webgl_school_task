534141ed94c627bc49a849b74a30690086117def
igarashi <digarashi91@gmail.com>
2021-06-17 23:50:14 +0900

    [modify] エフェクトを追加（途中）

--- 2/exercise/script.js
+++ 2/exercise/script.js
@@ -25,11 +25,26 @@
     let renderer;
     let camera;
     let geometry;
-    let materialPoint;
-    let points
+    let material;
+    let plane;
+    let sphere;
+    let bpx;
+    let torus;
+    let group;
+    let directionalLight;
+    let ambientLight;
+    let texture;
+    let composer;
+    let renderPass;
+    let glitchPass;
     let controls;
     let axesHelper;
 
+    const SCENE_PARAM = {
+        fogColor: 0xffffff,
+        fogNear: 1.0,
+        forFar: 15.0
+    };
 
     const CAMERA_PARAM = {
         fovy: 60,
@@ -54,11 +69,29 @@
         sizeAttenuation: true
     };
 
+    const DIRECTIONAL_LIGHT_PARAM = {
+        color: 0xffffff,
+        intensity: 1.0,
+        x: 1.0,
+        y: 1.0,
+        z: 1.0,
+    };
+
+    const AMBIENT_LIGHT_PARAM = {
+        color: 0xffffff,
+        intensity: 0.2
+    };
+
     function init() {
 
         wrapper = document.getElementById('webgl');
 
         scene = new THREE.Scene();
+        scene.fog = new THREE.Fog(
+            SCENE_PARAM.fogColor,
+            SCENE_PARAM.fogNear,
+            SCENE_PARAM.fogFar,
+        );
 
         renderer = new THREE.WebGLRenderer();
         renderer.setClearColor(RENDERER_PARAM.clearColor);
@@ -81,27 +114,56 @@
         );
         camera.lookAt(CAMERA_PARAM.lookAt);
 
-        materialPoint = new THREE.PointsMaterial(MATERIAL_PARAM);
+        composer = new THREE.EffectComposer(renderer);
+        renderPass = new THREE.RenderPass(scene, camera);
+        composer.addPass(renderPass);
+        glitchPass = new THREE.GlitchPass();
+        composer.addPass(glitchPass);
+        glitchPass.renderToScreen = true;
 
-        geometry = new THREE.BufferGeometry();
-        const COUNT = 10000;
-        const SIZE = 20.0;
-        const vertices = [];
+        group = new THREE.Group();
 
-        for (let i = 0; i < COUNT; ++i) {
-            const x = (Math.random() - 0.5) * 2.0 * SIZE;
-            const y = (Math.random() - 0.5) * 2.0 * SIZE;
-            const z = (Math.random() - 0.5) * 2.0 * SIZE;
-            vertices.push(x, y, z);
-        }
+        material = new THREE.MeshPhongMaterial(MATERIAL_PARAM);
+        material.map = texture;
 
-        const stride = 3;
-        const attribute = new THREE.BufferAttribute(new Float32Array(vertices), stride);
+        geometry = new THREE.BufferGeometry(2.0, 2.0);
+        plane = new THREE.Mesh(geometry, material);
+        plane.position.set(-2.0, 0.0, 2.0);
+        group.add(plane);
 
-        geometry.setAttribute('position', attribute);
+        geometry = new THREE.PlaneGeometry(1.0, 16, 16);
+        sphere = new THREE.Mesh(geometry, material);
+        sphere.position.set(2.0, 0.0, 2.0);
+        group.add(sphere);
 
-        points = new THREE.Points(geometry, materialPoint);
-        scene.add(points);
+        geometry = new THREE.BoxGeometry(1.0, 2.0, 3.0);
+        box = new THREE.Mesh(geometry, material);
+        box.position.set(2.0, 0.0, -2.0);
+        group.add(box);
+
+        geometry = new THREE.TorusGeometry(1.0, 0.4, 32, 32);
+        torus = new THREE.Mesh(geometry, material);
+        torus.position.set(-2.0, 0.0, -2.0);
+        group.add(torus);
+
+        scene.add(group);
+
+        directionalLight = new THREE.DirectionalLight(
+            DIRECTIONAL_LIGHT_PARAM.color,
+            DIRECTIONAL_LIGHT_PARAM.intensity,
+        );
+        directionalLight.position.set(
+            DIRECTIONAL_LIGHT_PARAM.x,
+            DIRECTIONAL_LIGHT_PARAM.y,
+            DIRECTIONAL_LIGHT_PARAM.z,
+        );
+        scene.add(directionalLight);
+
+        ambientLight = new THREE.AmbientLight(
+            AMBIENT_LIGHT_PARAM.color,
+            AMBIENT_LIGHT_PARAM.intensity
+        );
+        scene.add(ambientLight);
 
         controls = new THREE.OrbitControls(camera, renderer.domElement);
 
@@ -122,8 +184,9 @@
 
         controls.update();
 
-        points.rotation.x += 0.00025;
-        points.rotation.y += 0.00025;
+        if (isActive) {
+            group.rotation.y += 0.2;
+        }
 
         // scene.rotation.x += 0.0003;
         // scene.rotation.y += 0.0007;
